dabbc15769e1b02f326321f2f213f3caeadd2ce5
Nicolas Kolling Ribas <nicolaskribas@gmail.com>
2023-09-11 18:36:09 -0300

    Developing

--- src/bpfwavelet.bpf.c
+++ src/bpfwavelet.bpf.c
@@ -4,23 +4,29 @@
 
 char __license[] SEC("license") = "GPL";
 
-#define MAX_LEVELS 29
+#define MAX_LEVELS 256
 
+// array of lenght 1, because the time must be inside a map
 struct {
-	__uint(type, BPF_MAP_TYPE_HASH);
+	__uint(type, BPF_MAP_TYPE_ARRAY);
 	__uint(max_entries, 1);
-	__type(key, u16);
+	__type(key, u32);
 	__type(value, struct counter);
 } flow SEC(".maps");
 
-int alpha;
-int beta;
+// this variables are set in the userspace code
+__u64 alpha;
+__u64 beta;
+__u64 nsecs;
+__u8 levels;
+
+static bool timer_was_init = false;
+static u64 pkt_count = 0;
+static u64 sample_idx = 0;
+static u64 w[MAX_LEVELS] = { 0 };
+static u64 s[MAX_LEVELS - 1] = { 0 };
 
 struct counter {
-	u64 pkt_count;
-	u64 sample_idx;
-	u64 w[MAX_LEVELS];
-	u64 s[MAX_LEVELS - 1];
 	struct bpf_timer timer;
 };
 
@@ -29,37 +35,40 @@ static int calculate(void *map, int *key, struct counter *val)
 	u8 j;
 	u64 x, k;
 
-	bpf_timer_start(&val->timer, 1000000000, 0);
-	x = __sync_fetch_and_and(&val->pkt_count, 0); // atomically read then set to 0
-	k = val->sample_idx;
+	bpf_timer_start(&val->timer, nsecs, 0);
+	x = __sync_fetch_and_and(&pkt_count, 0); // atomically read then set to 0
+	k = sample_idx;
 
 	bpf_printk("count:%d", x);
 	bpf_printk("i:%d", k);
 
-	for (j = 0; j < MAX_LEVELS; j++) {
+	for (j = 0; j < levels; j++) {
 		if (k % 2 == 0) {
-			val->w[j] = x;
+			w[j] = x;
+			break;
 		} else {
+			bpf_printk("caclulating with %ld %ld", w[j], x);
 			if (j != 0) { // dont calculate energy for lvl 0 (signal)
-				val->s[j - 1] += (val->w[j] - x) * (val->w[j] - x);
+				s[j - 1] += (w[j] - x) * (w[j] - x);
 
 				if (j >= 2) { // compare with prev only lvl 2 onwards
-					if (2 * alpha * val->s[j - 1] < beta * val->s[j - 2]) {
+					if (2 * alpha * s[j - 1] < beta * s[j - 2]) {
 						bpf_printk("periodicity detected lvl %ld", j);
 					}
 				}
 			}
 
-			x = val->w[j] + x;
+			x += w[j];
+			bpf_printk("result x %ld", x);
 			k >>= 1;
 		}
 	}
 
-	for (j = 1; j < MAX_LEVELS; j++) {
-		bpf_printk(" [%d]:%ld", j, val->s[j - 1]);
+	for (j = 1; j < levels; j++) {
+		bpf_printk(" [%d]:%ld", j, s[j - 1]);
 	}
 
-	val->sample_idx++;
+	sample_idx++;
 	return 0;
 }
 
@@ -70,23 +79,17 @@ int xdp_pass(struct xdp_md *ctx)
 	void *data_end = (void *)(long)ctx->data_end;
 
 	int pkt_sz = data_end - data;
-	int idx = 0;
 
-	struct counter empty_counter = { 0 };
-
-	struct counter *fldt = bpf_map_lookup_elem(&flow, &idx);
+	int key = 0;
+	struct counter *fldt = bpf_map_lookup_elem(&flow, &key);
 	if (fldt) {
-		__sync_add_and_fetch(&fldt->pkt_count, 1); // atomically increment by 1
-	} else {
-		// for initialization the timer must be already in a map
-		// so we insert, retrive and then initialize
-		bpf_map_update_elem(&flow, &idx, &empty_counter, BPF_NOEXIST);
-		fldt = bpf_map_lookup_elem(&flow, &idx);
-		if (fldt) {
+		if (!timer_was_init) {
 			bpf_timer_init(&fldt->timer, &flow, 1);
 			bpf_timer_set_callback(&fldt->timer, calculate);
-			bpf_timer_start(&fldt->timer, 1000000000, 0);
+			bpf_timer_start(&fldt->timer, nsecs, 0);
+			timer_was_init = true;
 		}
+		__sync_add_and_fetch(&pkt_count, 1); // atomically increment by 1
 	}
 
 	return XDP_PASS;
--- src/bpfwavelet.c
+++ src/bpfwavelet.c
@@ -2,15 +2,24 @@
 #include "bpfwavelet.skel.h"
 #include <bpf/libbpf.h>
 #include <linux/if_link.h>
-
 #include <net/if.h>
 #include <signal.h>
-
 #include <unistd.h>
+
+#define DEFAULT_ALPHA	 3
+#define DEFAULT_BETA	 2
+#define DEFAULT_INTERVAL 1000000000 // 1 second
+#define DEFAULT_LEVELS	 17
+
+static bool verbose = false;
 
 static int libbpf_print_fn(enum libbpf_print_level level, const char *format, va_list args)
 {
-	return vfprintf(stderr, format, args);
+	if (verbose || level == LIBBPF_WARN) {
+		return vfprintf(stderr, format, args);
+	}
+
+	return 0;
 }
 
 static volatile bool exiting = false;
@@ -19,19 +28,103 @@ static void sig_handler(int sig)
 	exiting = true;
 }
 
+void usage(char *prog_name)
+{
+	fprintf(stderr,
+		"usage: %s [options] <ifname>\n"
+		"options:\n"
+		"  -v \n"
+		"  -a  set alpha value\n"
+		"  -b  set beta value\n"
+		"  -t  set the interval (in nanoseconds) between samples\n"
+		"  -l  set the number of decomposition levels\n",
+		prog_name);
+}
+
 int main(int argc, char **argv)
 {
 	struct bpfwavelet_bpf *skel;
 	int err;
+	int read;
 	int ifindex;
 	int prog_fd;
 	bool attached = false;
+	int c;
 
-	if (argc > 2) {
-		fprintf(stderr, "Too many arguments supplied.\n");
+	__u64 alpha = DEFAULT_ALPHA;
+	__u64 beta = DEFAULT_BETA;
+	__u64 interval = DEFAULT_INTERVAL;
+	__u8 levels = DEFAULT_LEVELS;
+
+	while ((c = getopt(argc, argv, ":va:b:t:l:")) != -1) {
+		switch (c) {
+		case 'v':
+			verbose = true;
+			break;
+		case 'a':
+			read = sscanf(optarg, "%llu", &alpha);
+			if (read != 1) {
+				fprintf(stderr, "error: could not parse argument for '-%c'\n",
+					optopt);
+				usage(argv[0]);
+				return 1;
+			}
+			break;
+		case 'b':
+			read = sscanf(optarg, "%llu", &beta);
+			if (read != 1) {
+				fprintf(stderr, "error: could not parse argument for '-%c'\n",
+					optopt);
+				usage(argv[0]);
+				return 1;
+			}
+			break;
+		case 't':
+			read = sscanf(optarg, "%llu", &interval);
+			if (read != 1) {
+				fprintf(stderr, "error: could not parse argument for '-%c'\n",
+					optopt);
+				usage(argv[0]);
+				return 1;
+			}
+			break;
+		case 'l':
+			read = sscanf(optarg, "%hhu", &levels);
+			if (read != 1) {
+				fprintf(stderr, "error: could not parse argument for '-%c'\n",
+					optopt);
+				usage(argv[0]);
+				return 1;
+			}
+			break;
+		case ':':
+			fprintf(stderr, "error: option '-%c' needs an argument\n", optopt);
+			usage(argv[0]);
+			return 1;
+			break;
+		case '?':
+			fprintf(stderr, "error: unrecognized option '-%c'\n", optopt);
+			usage(argv[0]);
+			return 1;
+			break;
+		default:
+			break;
+		}
+	}
+	if (optind >= argc) {
+		fprintf(stderr, "error: no ifname supplied\n");
+		usage(argv[0]);
 		return 1;
-	} else if (argc == 1) {
-		fprintf(stderr, "Expecting ifname as argument.\n");
+	} else if (optind != argc - 1) { // is last argument
+		fprintf(stderr, "error: too many arguments supplied\n");
+		usage(argv[0]);
+		return 1;
+	}
+
+	ifindex = if_nametoindex(argv[optind]);
+	if (ifindex == 0) {
+		fprintf(stderr, "error: no interface named '%s' found. %s\n", argv[optind],
+			strerror(errno));
 		return 1;
 	}
 
@@ -40,30 +133,30 @@ int main(int argc, char **argv)
 	signal(SIGINT, sig_handler);
 	signal(SIGTERM, sig_handler);
 
-	ifindex = if_nametoindex(argv[1]);
-	if (ifindex == 0) {
-		fprintf(stderr, "No interface found with given name. %s\n", strerror(errno));
-		return 1;
-	}
-
 	skel = bpfwavelet_bpf__open_and_load();
 	if (skel == NULL) {
-		fprintf(stderr, "Failed to open or load BPF program. %s\n", strerror(errno));
+		fprintf(stderr, "error: failed to open or load BPF program. %s\n", strerror(errno));
 		goto cleanup;
 	}
 
-	skel->bss->beta = 3;
-	skel->bss->alpha = 2;
+	skel->bss->alpha = alpha;
+	skel->bss->beta = beta;
+	skel->bss->nsecs = interval;
+	skel->bss->levels = levels;
 
 	prog_fd = bpf_program__fd(skel->progs.xdp_pass);
 	err = bpf_xdp_attach(ifindex, prog_fd, XDP_FLAGS_SKB_MODE, NULL);
 	if (err) {
-		fprintf(stderr, "Failed to attach: %d\n", err);
+		fprintf(stderr, "error: failed to attach BPF program: %d\n", err);
 		goto cleanup;
 	}
 	attached = true;
 
-	fprintf(stderr, "Started!\n");
+	if (verbose) {
+		fprintf(stderr, "alpha: %llu  beta: %llu  interval: %llu  levels: %d\n", alpha,
+			beta, interval, levels);
+	}
+	fprintf(stderr, "running");
 
 	while (!exiting) {
 		fprintf(stderr, ".");
