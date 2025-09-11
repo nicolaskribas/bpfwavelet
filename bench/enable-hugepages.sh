dc00062d67280adcacfa0063eedc99f2f6dfbc84
Nicolas Kolling Ribas <nicolaskribas@gmail.com>
2025-09-11 16:22:12 -0300

    style

--- bench/enable-hugepages.sh
+++ bench/enable-hugepages.sh
@@ -4,14 +4,14 @@ trap 'echo "$0: Error on line ${LINENO}: ${BASH_COMMAND}" >&2' ERR
 
 # modified from https://github.com/tohojo/xdp-paper/blob/master/benchmarks/enable_huge_pages.sh
 
-DOC='For DPDK, enable huge pages'
+doc='For DPDK, enable huge pages'
 
-if [ "${EUID:-$(id -u)}" -ne 0 ]; then
+if ((EUID)); then
 	echo "Need root priviliges" >&2
 	exit 1
 fi
 
-echo "$DOC"
+echo "$doc"
 
 echo 1024 >/sys/kernel/mm/hugepages/hugepages-2048kB/nr_hugepages
 
